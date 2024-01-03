1501aa0d5f0b2e4af88e44458207c85f83ee4c42
Harikaran <harikaran7242@gmail.com>
2024-01-03 22:40:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Task-7
+
+* The script uses `XMLHttpRequest` to make a `GET request` to the Restcountries API and fetches country data.
+* Various array functions are applied to the fetched data to perform tasks such as filtering countries, calculating total population, and identifying countries using US dollars as currency.
+
+ **Countries in Asia:**
+*Filter and display countries from the Asia continent.*
+
+**Countries with Population Less Than 2 Lakhs:**
+*Filter and display countries with a population of less than 2 lakhs.*
+
+**Print Country Details Using forEach:**
+*Print country details including name, capital, and flag using the forEach function.*
+
+**Total Population Calculation Using Reduce:**
+*Calculate and display the total population of all countries using the reduce function.*
+
+**Countries Using US Dollars:**
+*Identify and display countries that use US dollars as currency.*
---
+++ script.js
@@ -0,0 +1,50 @@
+const xhr = new XMLHttpRequest();
+
+
+xhr.open("GET", "https://restcountries.com/v3.1/all");
+
+xhr.onload = function () {
+  if (xhr.status >= 200 && xhr.status < 300) {
+ 
+    const data = JSON.parse(xhr.responseText);
+
+    // a. Get all the countries from Asia continent/region using Filter function
+    const asiaCountries = data.filter((country) =>
+      country.region.includes("Asia")
+    );
+    console.log("Countries in Asia:", asiaCountries);
+
+    // b. Get all the countries with a population of less than 2 lakhs using Filter function
+    const lessThan2LakhPopulation = data.filter(
+      (country) => country.population < 200000
+    );
+    console.log(
+      "Countries with population less than 2 lakhs:",
+      lessThan2LakhPopulation
+    );
+
+    // c. Print the details (name, capital, flag) using forEach function
+    data.forEach((country) => {
+      console.log(
+        `Name: ${country.name.common}, Capital: ${country.capital}, Flag: ${country.flags.png}`
+      );
+    });
+
+    // d. Print the total population of countries using reduce function
+    const totalPopulation = data.reduce(
+      (acc, country) => acc + country.population,
+      0
+    );
+    console.log("Total population of countries:", totalPopulation);
+
+    // e. Print the country that uses US dollars as currency
+    const usDollarCountries = data.filter(
+      (country) => country.currencies && country.currencies.USD
+    );
+    console.log("Countries using US dollars:", usDollarCountries);
+  } 
+};
+
+
+// Send the request
+xhr.send();
